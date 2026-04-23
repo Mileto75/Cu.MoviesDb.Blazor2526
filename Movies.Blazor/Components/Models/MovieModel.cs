0e2b5ba9b3787d36798c9b4ec2dfcb39fbbbc52a
Mileto Di Marco <mileto.di.marco@howest.be>
2026-04-23 09:53:46 +0200

    Add movies page

---
+++ Movies.Blazor/Components/Models/MovieModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Movies.Blazor.Components.Models
+{
+    public class MovieModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Image { get; set; }
+    }
+}
